84f5e61ccb7c99a938db502f6e20404312e751a0
Jonah Elliott <jonahelliott@u.boisestate.edu>
2025-11-09 01:36:17 +0000

    Made changes to improve readability

--- dist/script.js
+++ dist/script.js
@@ -1,35 +1,42 @@
+//Initialize variables
 let textBox = document.getElementById('textBox');
 
 const addBtn = document.getElementById('addBtn');
 
-
-//const addBtn = document.querySelector('addBtn');
+//Adds tasks to a list and registers an event handler for closing the tasks
 function addTask(task) {
-    console.log("ran function 'addTask'");
+    //Store a new task onto a variable, change its HTML, and append it to the list
     let newTask = document.createElement('li');
     newTask.innerHTML = '<span class="task-text">' + task + '</span><button class="done-btn">&#10006;</button>';
     document.querySelector('ol').appendChild(newTask);
+    //Create a list of all done buttons
     let doneBtnList = document.querySelectorAll('.done-btn');
+    //Find the last done button in the list and add an event listener to it
     doneBtnList[doneBtnList.length - 1].addEventListener("click", removeTask);
+    console.log("ran function 'addTask'");
 }
 
+//Code executed upon clicking the "Add" button or releasing the "Enter" key
 function addBtnClick(event) {
     let text = textBox.value;
-    if (text !== "") {
+    //Do not add the task if the input is empty
+    if (text != "") {
         addTask(textBox.value);
         textBox.value = "";
         textBox.focus();
         console.log("ran function 'addBtnClick'");
     } else {
-        console.log("Cannot create task: text box input is empty");
+        console.log("cannot create task: text box input is empty");
     }
 }
 
+//Called to remove a task when the 'x' is clicked
 function removeTask(event) {
     event.target.parentNode.parentNode.removeChild(event.target.parentNode);
     console.log("ran function 'removeTask'");
 }
 
+//Calls addBtnClick() when the 'Enter' key is released
 function textBoxEnter(event) {
     if (event.key == "Enter") {
         addBtnClick();
@@ -37,14 +44,18 @@ function textBoxEnter(event) {
     console.log("ran function 'textBoxEnter'");
 }
 
+//Enables the event handlers after the DOM is loaded
 function domLoaded() {
     addBtn.addEventListener("click", addBtnClick);
     textBox.addEventListener("keyup", textBoxEnter);
     console.log("ran function 'domLoaded'");
 }
 
-domLoaded();
+//Check if the DOM is fully loaded before running any functions
+document.addEventListener("DOMContentLoaded", domLoaded);
 
+
+/* Starter code js */
 // // This is a simple JavaScript file that adds interactivity to the HTML page
 // // It defines a function to show an alert when a link is clicked
 // function sayHello() {
